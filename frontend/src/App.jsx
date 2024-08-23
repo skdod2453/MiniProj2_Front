93b15a2eccbd3d7ed1ef6e93588a3a9e4d0ba4bd
mgs06380 <twins2479@gmail.com>
2024-08-23 14:08:43 +0900

    authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import './App.css';
+import { AuthProvider } from './context/AuthContext';
+import Register from './pages/Register';
+import Login from './pages/Login';
+//import Home from './pages/Home';
+//import Profile from './pages/Profile';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ChakraProvider>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            {/*<Route path='/home' element={<Home />} />
+            <Route path='/profile/:userId' element={<Profile />} /> */}
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
